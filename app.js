e3db5f33c3f66a54b8009f3e68c8655f6cb82bf4
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-01 15:50:27 -0700

    corrected some errors

--- app.js
+++ app.js
@@ -75,18 +75,24 @@ $(document).ready(function() {
 			url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchValue + '&appid=' + apikey,
             dataType: 'json',
             success: function(data){
-                $('#forecast').html('<h4 class="> 5-Day Forecast:</h4>').append('<div class="row">');
+                $('#forecast').html('<h5 class="mt-3"> 5-Day Forecast:</h5>').append('<div class="row">');
 
                 //loop over all forecasts
                 for (var i = 0; i < data.list.length; i++) {
                     if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
                         var col = $('<div>').addClass('col-md-2');
+
                         var card = $('<div>').addClass('card bg-secondary text-white');
+
                         var body = $('<div>').addClass('card-body p-2');
-                        var title = $('<h4>').addClass('card-text').text(new Date(data.list[i].dt_txt).toLocaleDateString());
+
+                        var title = $('<h5>').addClass('card-title').text(new Date(data.list[i].dt_txt).toLocaleDateString());
+
                         var img = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + data.list[1]. weather[0].icon + '.png'
                         );
+
                         var p1 = $('<p>').addClass('card-text').text('Temp: ' + data.list[i].main.temp_max + ' °F');
+
                         var p2 = $('<p>').addClass('card-text').text('humidity: ' + data.list[i].main.humidity + '%');
 
                         col.append(card.append(body.append(title, img, p1, p2)));
@@ -111,7 +117,7 @@ function getUVIndex(lat, lon) {
         success: function(data) {
             var uv = $('<p>').text('UV Index: ');
             var btn = $('<span>').addClass('btn btn-sm').text(data.value);
-
+// color changes based on weather condition
             if(data.value < 3) {
                 btn.addClass('btn-success');
                 } else if(data.value < 7) {
@@ -124,6 +130,7 @@ function getUVIndex(lat, lon) {
         },
     });
 }
+// function built to store last city searched in localStorage
 var history = JSON.parse(window.localStorage.getItem('history')) || [];
 if (history.length > 0) {
     searchWeather(history[history.length - 1]);
